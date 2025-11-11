52ce0562ddfd411d7fa3b19dfa30458245742e17
Qingling Wu <qingling.wu@nxp.com>
2025-11-11 18:34:37 +0800

    Fix DFS status check failure when GO and STA were on different channels
    [Description]
    Failed to check DFS status when GO and STA were on different channels, GO on channel 36 and STA on channel 6
    
    [Root Cause]
    For P2P GO, hapd->iface->current_mode->mode is HOSTAPD_MODE_IEEE80211A for channel 36. After STA connect to external AP on channel, GO switch to the new channel 6, but hapd->iface->current_mode->mode was not updated according to new channel, so cause this failure.
    
    [Fix]
    In hostapd_event_ch_switch(), update hw_mode according to the channel when CSA finished, then could cover the cases where the driver changed the operating band, keep consistent with the open source hostap code.
    
    [Testing]
    Local test ok on RW610 and IW610
    
    [Reviewers]
    Gannicus Guo, Sanjay Yadav
    
    [Port proposal]
    non-general
    
    Signed-off-by: Qingling Wu <qingling.wu@nxp.com>

--- src/ap/hw_features.h
+++ src/ap/hw_features.h
@@ -14,6 +14,7 @@
 #ifdef NEED_AP_MLME
 void hostapd_free_hw_features(struct hostapd_hw_modes *hw_features, size_t num_hw_features);
 int hostapd_get_hw_features(struct hostapd_iface *iface);
+int hostapd_csa_update_hwmode(struct hostapd_iface *iface);
 int hostapd_acs_completed(struct hostapd_iface *iface, int err);
 int hostapd_select_hw_mode(struct hostapd_iface *iface);
 const char *hostapd_hw_mode_txt(int mode);
@@ -25,6 +26,7 @@ int hostapd_check_he_6ghz_capab(struct hostapd_iface *iface);
 int hostapd_prepare_rates(struct hostapd_iface *iface, struct hostapd_hw_modes *mode);
 void hostapd_stop_setup_timers(struct hostapd_iface *iface);
 int hostapd_hw_skip_mode(struct hostapd_iface *iface, struct hostapd_hw_modes *mode);
+int hostapd_determine_mode(struct hostapd_iface *iface);
 #else /* NEED_AP_MLME */
 static inline void hostapd_free_hw_features(struct hostapd_hw_modes *hw_features, size_t num_hw_features)
 {
@@ -33,6 +35,11 @@ static inline void hostapd_free_hw_features(struct hostapd_hw_modes *hw_features
 static inline int hostapd_get_hw_features(struct hostapd_iface *iface)
 {
     return -1;
+}
+
+static inline int hostapd_csa_update_hwmode(struct hostapd_iface *iface)
+{
+    return 0;
 }
 
 static inline int hostapd_acs_completed(struct hostapd_iface *iface, int err)
@@ -80,6 +87,11 @@ static inline int hostapd_hw_skip_mode(struct hostapd_iface *iface, struct hosta
 }
 
 static inline int hostapd_check_he_6ghz_capab(struct hostapd_iface *iface)
+{
+    return 0;
+}
+
+static inline int hostapd_determine_mode(struct hostapd_iface *iface)
 {
     return 0;
 }
